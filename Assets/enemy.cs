827ac53b9ac11059098f59ab76f84471b959ab48
nate <a24684841@gmail.com>
2021-05-12 16:08:09 +0800

    first commit

---
+++ Assets/enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+        rb.AddForce(( player.transform.position - transform.position).normalized * speed);
+
+
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody rb;
+    GameObject center;
+    public GameObject Indicator;
+    public GameObject Indicator2;
+    public float speed = 10;
+    bool hasPower = false;
+    public float force = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        center = GameObject.Find("center");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = Input.GetAxis("Vertical");
+        Debug.Log(value);
+        rb.AddForce(center.transform.forward * value * speed );
+        Indicator.gameObject.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        Indicator2.gameObject.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+    }
+    private void OnCollisionEnter(Collision col)
+
+    {
+        if(col.gameObject.CompareTag("enemy") && hasPower)
+        {
+
+            Rigidbody enbody = col.gameObject.GetComponent<Rigidbody>();
+            enbody.AddForce((col.gameObject.transform.position - transform.position)*force, ForceMode.Impulse);
+        }
+        if ( col.gameObject.CompareTag("food"))
+        {
+            hasPower = true;
+            Indicator.gameObject.SetActive(true);
+            Indicator2.gameObject.SetActive(true);
+            Destroy(col.gameObject);
+        }
+    }
+
+
+}
